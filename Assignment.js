e93f04ef25f9c2e1dd4fafee7df8304f71732e3a
LikhithaReddyS <80111028+LikhithaReddyS@users.noreply.github.com>
2021-08-03 16:15:06 +0530

    Create Assignment.js

---
+++ Assignment.js
@@ -0,0 +1,11 @@
+<script>
+let a=9;
+let b=7;
+let sum=a+b;
+let prod=a*b;
+let div=a/b;
+let sub=a-b;
+document.write(`The sum of a and b is ${sum} <br/> The difference of a and b is ${sub} <br/> The product of a and b is ${prod} <br/>
+The division of a and b is ${div}`)
+
+</script>
